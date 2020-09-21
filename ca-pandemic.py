4b165529753290be562e4166b28ddc9ea140a675
owren <palanders@owren.xyz>
2020-09-21 14:17:45 +0200

    implemented Person class and set_age() to get weighted random age

--- ca-pandemic.py
+++ ca-pandemic.py
@@ -1,8 +1,34 @@
 import pycxsimulator
 from pylab import *
+import pandas as pd
+import numpy as np
+import random
 
 n = 100 # size of space: n x n
 p = 0.24 # probability of initially panicky individuals
+
+df = pd.read_csv('population2020.csv')
+age_array = df.values
+
+
+def set_age():
+    # statistics gotten from https://www.ssb.no/statbank/table/07459/ (21.09.2020)
+    weight = []
+    ages = []
+    for age_group in age_array:
+        weight.append(age_group[2])
+        ages.append([age_group[0], age_group[1]])
+    age_range = random.choices(ages, weights=weight, k=1)
+    age = random.randint(age_range[0][0], age_range[0][1])
+    return age
+
+
+class Person:
+    age = set_age()
+    chance_of_bc = 0.10
+    background_sickness = 1 if random.uniform(0, 1) < chance_of_bc else 0
+    exposure = random.uniform(0, 1)
+
 
 def initialize():
     global config, nextconfig
@@ -12,10 +38,12 @@ def initialize():
             config[x, y] = 1 if random() < p else 0
     nextconfig = zeros([n, n])
 
+
 def observe():
     global config, nextconfig
     cla()
     imshow(config, vmin = 0, vmax = 1, cmap = cm.binary)
+
 
 def update():
     global config, nextconfig
@@ -24,9 +52,8 @@ def update():
             count = 0
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
-                    # if 100 -> next is not 101, it is 0, both x and y direction
                     count += config[(x + dx) % n, (y + dy) % n]
             nextconfig[x, y] = 1 if count >= 4 else 0
     config, nextconfig = nextconfig, config
 
-pycxsimulator.GUI().start(func=[initialize, observe, update])
+#pycxsimulator.GUI().start(func=[initialize, observe, update])
