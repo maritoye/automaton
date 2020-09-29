13cc3fb2684c2148b87297cd9d7b2c5076f4bd6f
Shayan <shayan.yazdanmehr@nav.no>
2020-09-29 19:34:18 +0200

    refactoring

--- Types.py
+++ Types.py
@@ -11,38 +11,39 @@ class PersonState(Enum):
 
 
 class Gender(Enum):
-    MALE = 0,
+    MALE = 0
     FEMALE = 1
 
 
 class BackgroundSickness(Enum):
-    NO = 0,
-    SLIGHTLY_INCREASED = 1,
-    MODERATE = 2,
-    ABOVE_AVERAGE = 3,
-    HIGH = 4,
-    FATAL = 5
+    NO = 0
+    YES = 1
+    # SLIGHTLY_INCREASED = 1,
+    # MODERATE = 2,
+    # ABOVE_AVERAGE = 3,
+    # HIGH = 4,
+    # FATAL = 1
 
 
 class FollowProtocol(Enum):
     STRICT = 0
-    ABOVE_AVERAGE = 1,
-    MODERATE = 2,
-    SLIGHTLY = 3,
-    NO = 4,
+    ABOVE_AVERAGE = 1
+    MODERATE = 2
+    SLIGHTLY = 3
+    NO = 4
 
 
 class Quarantine(Enum):
-    NO = 0,
-    QUARANTINE = 1,
-    TOTAL_ISOLATION = 2,
+    NO = 0
+    QUARANTINE = 1
+    TOTAL_ISOLATION = 2
 
 
 class AgeGroup(Enum):
-    INFANT = range(0, 3),
-    CHILD = range(3, 18),
-    YOUNG_ADULT = range(18, 30),
-    ADULT = range(30, 50),
-    MIDDLE_AGE = range(50, 70),
-    AGED = range(70, 90),
-    OLD = range(90, 110),
+    INFANT = range(0, 3)
+    CHILD = range(3, 18)
+    YOUNG_ADULT = range(18, 30)
+    ADULT = range(30, 50)
+    MIDDLE_AGE = range(50, 70)
+    AGED = range(70, 90)
+    OLD = range(90, 110)
