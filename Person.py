a4711dc58d5ddfe10cc724def22304b93f4630cc
maritoye <maritg@me.com>
2020-10-05 09:40:17 +0200

    creating get_risk_ratio, a fuction to determin if person can infect others

--- Person.py
+++ Person.py
@@ -16,7 +16,7 @@ class Person:
     state_count = 0
     gender = 0
     exposure_ratio = 0
-    vulnerability_ratio = 0
+    vulnerability_ratio = 0 
     death_ratio = 0
     incubation_period = 0  # 2-14 (3-5 har mye vekt)
     bmi = 0
@@ -42,20 +42,52 @@ class Person:
         self.state_count = 0
 
     def get_vulnerability_ratio(self):
+        #risk of getting infection
+        
         pass
 
-    def get_risk_ratio(self):
+    def get_risk_ratio(self, mask, distancing, hygiene, curfew):
+        # risk of infecting others
         if self.person_state not in [PersonState.SICK, PersonState.INFECTIOUS]:
             return 0
         else:
             if self.quarantine == Quarantine.TOTAL_ISOLATION:
-                return 0
-            # TODO or Not TODO?
-            # elif self.quarantine == Quarantine.QUARANTINE:
-            # if(self.follow_protocol == )
+                # any chance of people breaking isolation?
+                # what about people you live with? are they also isolated, or quarantined
+                # in those cases they can be infected...
+                return 0 
+            
+            # TODO or Not TODO? That is the question
+            
+            elif self.quarantine == Quarantine.QUARANTINE:
+            # how strict is the quarantine (can you go to stores, be with others)
+            # how strictly do you follow protocols?
+                #if(self.follow_protocol == )
+         
+        #if infected or sick but not in quarantine or isolation
+        else: 
+            protocols = (distancing + mask + hygiene)/3
+            # social distancing rules =
+            # face mask rules / recommendations
+            # + distancing (i.e one meter rule)    
+            # + hygiene rules 
+            
+            risk_ratio = protocols * self.follow_protocol
+            risk ratio = risk_ratio * self.exposure
+            # following protocols * social distancing rules
+            # + exposure??
+            # + staying at home rules (i.e portforbud) (can be part of the exposure variable??)
+            return risk_ratio
+            
 
         pass
-
+    
+    def get_death_ratio(self):
+        # background_sickness, maybe age, lifestyle (smoking, weight), health care (respirator, medicines)
+        # https://www.fhi.no/contentassets/8a971e7b0a3c4a06bdbf381ab52e6157/vedlegg/andre-halvar-2020/2020.09.23-ukerapport-uke-38-covid-19.pdf
+        # page 24
+        pass
+    
     def print_self(self):
         print('Age =', self.age)
         print('bs =', self.background_sickness)
