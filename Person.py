ab40d5258c170204726d561dd9af20b3496b4c5c
owren <palanders@owren.xyz>
2020-10-12 09:32:47 +0200

    implemented get_adherence(), pulled from other branch

--- Person.py
+++ Person.py
@@ -1,68 +1,96 @@
-from PersonState import PersonState
-import pandas as pd
+from Types import PersonState, Quarantine, Gender
 import random
-
-n = 10
-p = 0.1
-
-df = pd.read_csv('population2020.csv')
-age_array = df.values
+import utils
 
 
 class Person:
     age = 0
+    age_group = 0
     background_sickness = 0
-    exposure = 0
+    exposure_radius = 0  # How active one person is
     follow_protocol = 0
     quarantine = 0
     state = PersonState.EMPTY
-    state_count = 0
+    gender = 0
+    exposure_ratio = 0
     vulnerability_ratio = 0
+    death_ratio = 0
+    incubation_period = 0  # 2-14 (3-5 har mye vekt) vanligvis ikke over 9
+    recovery_period = 0
+    bmi = 0
+    smoking = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, chance_of_infection):
+        self.age = utils.set_age()
+        self.age_group = utils.get_age_group(self.age)
+        self.background_sickness = utils.get_background_sickness(self.age)
+        # self.exposure = random.randint(1, 3) # how big (1 - one ring, 3 - three rings) the ´social ring´ is for current person
+        self.exposure_radius = random.randint(1, 3)
 
-
-    def set_values(self):
-        self.age = self.set_age()
-        # TODO: increase bs when older age
-        chance_of_bs = 0.10
-        self.background_sickness = 1 if random.uniform(0, 1) < chance_of_bs else 0
-        # TODO: increase exposure depending on age
-        self.exposure = random.uniform(0, 1)
         # TODO: increase follow_protocol depending on age
-        self.follow_protocol = random.uniform(0, 1)
+        self.follow_protocol = random.uniform(0.5, 1)
         self.quarantine = 0
+        self.gender = Gender.FEMALE if random.random() < 0.5 else Gender.MALE
 
-        chance_of_inf = p
-        #self.state = states[1] if random.uniform(0, 1) < chance_of_inf else states[0]
-        self.state = PersonState.INFECTIOUS if random.uniform(0, 1) < chance_of_inf else PersonState.HEALTHY
-        self.state_count = 0
+        # self.state = states[1] if random.uniform(0, 1) < chance_of_inf else states[0]
+        self.state = PersonState.INFECTIOUS if random.uniform(0, 1) < chance_of_infection else PersonState.HEALTHY
+        self.incubation_period = random.randint(2, 14)
+        self.recovery_period = random.randint(7, 28)
 
+    def get_vulnerability_ratio(self, mask, distancing, hygiene, curfew):
+        # risk of getting infected  0 no risk, 1 = highest risk
+        vulnerability_ratio = (mask + distancing + hygiene + curfew) / 4
+        return (1 - vulnerability_ratio) * self.follow_protocol
 
-    def set_age(self):
-        """
-        Sets a random weighted age for the person
-        Data collected from ssb.no to get realistic ages
-            collected from: https://www.ssb.no/statbank/table/07459/
-            date: (21.09.2020)
-        ----------
-        Return:
-            age - int, the age of the person
-        """
-        weight = []
-        ages = []
-        for group in age_array:
-            weight.append(group[1])
-            ages.append(group[0])
-        age = int(random.choices(ages, weights=weight, k=1)[0])
-        return age
+    def get_risk_ratio(self, mask, distancing, hygiene, curfew):
+        # risk of infecting others 0 no risk, 1 = highest risk
+        if self.state not in [PersonState.SICK, PersonState.INFECTIOUS]:
+            return 0
+        else:
+            if self.quarantine == Quarantine.TOTAL_ISOLATION:
+                # any chance of people breaking isolation?
+                # what about people you live with? are they also isolated, or quarantined
+                # in those cases they can be infected...
+                return 0
 
+                # TODO or Not TODO? That is the question
+
+            elif self.quarantine == Quarantine.QUARANTINE:
+                # how strict is the quarantine? (can you go to stores, be with others)
+                # how strictly do you follow protocols?
+                return 1 - (self.follow_protocol / 2)
+
+            # if infected or sick but not in quarantine or isolation
+            else:
+                protocols = (distancing + mask + hygiene) / 3
+                # social distancing rules =
+                # face mask rules / recommendations
+                # + distancing (i.e one meter rule)
+                # + hygiene rules
+
+                risk_ratio = (1 - protocols) * (1 - self.follow_protocol)
+                # following protocols * social distancing rules
+                # + exposure??
+                # + staying at home rules (i.e portforbud) (can be part of the exposure variable??)
+                return risk_ratio
+
+    def get_death_ratio(self):
+        # background_sickness, maybe age, lifestyle (smoking, weight), health care (respirator, medicines)
+        # https://www.fhi.no/contentassets/8a971e7b0a3c4a06bdbf381ab52e6157/vedlegg/andre-halvar-2020/2020.09.23-ukerapport-uke-38-covid-19.pdf
+        # page 24
+        # 1.38% som får corona dør av det. 87% av desse har underliggende sykdom
+        if self.background_sickness:
+            death_ratio = 0.0120006
+
+        else:
+            death_ratio = 0.001794
+
+        return death_ratio
 
     def print_self(self):
         print('Age =', self.age)
         print('bs =', self.background_sickness)
-        print('Exposure =', self.exposure)
+        print('Exposure =', self.exposure_radius)
         print('Follow protocol =', self.follow_protocol)
         print('In quarantine =', self.quarantine)
         print('State =', self.state)
--- PersonState.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class PersonState(Enum):
-    EMPTY = 0
-    HEALTHY = 1
-    INFECTIOUS = 2
-    SICK = 3
-    RECOVERED = 4
-    DEATH = 5
