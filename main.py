5be882ddc810fb83542ce44896c48dadee90f560
Shayan <shayan.yazdanmehr@nav.no>
2020-10-14 07:26:08 +0200

    Changing parameters in main and show brief statistic and score from fitness function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Population import Population
+from utils import fitness_function
 
-a = Population(x=50, y=50, healthcare=0.8, hygiene=0.8, mask=0.8, distancing=0.8, curfew=0.8, test_rate=0.3)
+a = Population(x=50, y=50, healthcare=0.1, hygiene=0.1, mask=0.1, distancing=0.1, curfew=0.1, test_rate=0.3)
 for i in range(100):
     a.update()
     if i == 0 or i % 10 == 0:
         print(i)
         a.observe()
+        print(a.get_brief_statistics())
+        print(fitness_function(a.get_brief_statistics()))
