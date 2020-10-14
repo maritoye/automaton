bc012494e7c91e685f16a16a9bcbdd642cce0687
Shayan <shayan.yazdanmehr@nav.no>
2020-10-14 20:45:43 +0200

    Tuning main and print statistic

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Population import Population
 from utils import fitness_function
 
-a = Population(x=50, y=50, healthcare=0.1, hygiene=0.1, mask=0.1, distancing=0.1, curfew=0.1, test_rate=0.3)
+a = Population(x=50, y=50, healthcare=0.6, hygiene=0.6, mask=0.6, distancing=0.6, curfew=0.6, test_rate=0.6)
 for i in range(100):
     a.update()
     if i == 0 or i % 10 == 0:
         print(i)
         a.observe()
         print(a.get_brief_statistics())
-        print(fitness_function(a.get_brief_statistics()))
+        print(a.get_statistics())
