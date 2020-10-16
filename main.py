329e1a55be2b6d1c4c1c37b59a63407f266b965e
Shayan <shayan.yazdanmehr@nav.no>
2020-10-16 11:01:10 +0200

    rename variable names

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Population import Population
 from utils import fitness_function
 
-a = Population(x=50, y=50, healthcare=0.6, hygiene=0.6, mask=0.6, distancing=0.6, curfew=0.6, test_rate=0.6)
-for i in range(100):
-    a.update()
-    if i == 0 or i % 10 == 0:
-        print(i)
-        a.observe()
-        print(a.get_brief_statistics())
-        print(a.get_statistics())
+firstPopulation = Population(x=50, y=50, healthcare=0.6, hygiene=0.6, mask=0.6, distancing=0.6, curfew=0.6, test_rate=0.6)
+for step in range(100):
+    firstPopulation.update()
+    if step == 0 or step % 10 == 0:
+        print(step)
+        firstPopulation.observe()
+        print(firstPopulation.get_brief_statistics())
+        print(firstPopulation.get_statistics())
