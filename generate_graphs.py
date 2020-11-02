85292d5d65687da4e1a1308740dc4ea4f7b3e767
maritoye <maritg@me.com>
2020-11-02 19:46:40 +0100

    create graph of persons in states per time step

---
+++ generate_graphs.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+
+def graph(healthy, infectious, sick, dead, recovered):
+	"""
+	creates a graph of the states of all Persons in a population
+	:param healthy: list - numbers of healthy Persons per timestep
+	:param infectious: list - numbers of infectious Persons per timestep
+	:param sick: list - numbers of sick Persons per timestep
+	:param dead: list - numbers of dead Persons per timestep
+	:param recovered: list - numbers of recovered Persons per timestep
+	:return:
+	"""
+	x = [i for i in range(len(healthy))]
+
+	plt.plot(x, healthy, label="Healthy", color="gray")
+	plt.plot(x, infectious, color="yellow", label="Infectious")
+	plt.plot(x, sick, color="red", label="Sick")
+	plt.plot(x, dead, color="black", label="Dead")
+	plt.plot(x, recovered, color="green", label="Recovered")
+
+	plt.xlabel('Time steps')
+	plt.ylabel('People')
+	plt.axis([0, len(x), 0, healthy[0]+infectious[0]])
+	plt.legend()
+
+	plt.savefig("one_run_images/graph.png")
+
+	plt.show()
+	plt.close()
