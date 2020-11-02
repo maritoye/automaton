4c6a014e7ba23ac720e55bdb429e8ac5180edd0c
maritoye <maritg@me.com>
2020-11-02 19:47:33 +0100

    make lists of people in states per time step, and call the graph maker

--- main_one_run.py
+++ main_one_run.py
@@ -1,14 +1,46 @@
 from GroupOfPeople import GroupOfPeople
 from Types import RulesIsolation, RulesQuarantine
+import generate_graphs
+import const
 
-firstPopulation = GroupOfPeople(x=50, y=50, healthcare=0.5, hygiene=0.5, mask=0.5, distancing=0.6, curfew=0.7,
-                                test_rate=0.6, quarantine_rules=RulesQuarantine.SICK_INFECTIOUS,
-                                isolation_rules=RulesIsolation.SICK_INFECTIOUS)
+X = 50
+Y = 50
+HEALTHCARE = 0.5
+HYGIENE = 0.5
+MASK = 0.5
+DISTANCING = 0.6
+CURFEW = 0.7
+TEST_RATE = 0.55
+QUARANTINE_RULES = RulesQuarantine.SICK_INFECTIOUS
+ISOLATION_RULES = RulesIsolation.SICK_INFECTIOUS
 
-for step in range(100):
+healthy = []
+infectious = []
+sick = []
+dead = []
+recovered = []
+
+firstPopulation = GroupOfPeople(x=X, y=Y, healthcare=HEALTHCARE, hygiene=HYGIENE, mask=MASK, distancing=DISTANCING,
+                                curfew=CURFEW, test_rate=TEST_RATE, quarantine_rules=QUARANTINE_RULES,
+                                isolation_rules=ISOLATION_RULES)
+
+for step in range(50):
     firstPopulation.update()
+    stats = firstPopulation.get_brief_statistics()
+    healthy.append(stats["healthy"])
+    infectious.append(stats["infectious"])
+    sick.append(stats["sick"])
+    dead.append(stats["dead"])
+    recovered.append(stats["recovered"])
+
     if step == 0 or step % 10 == 0:
         print(step)
-        firstPopulation.observe()
+        firstPopulation.observe(step)
         print(firstPopulation.get_brief_statistics())
-        print(firstPopulation.get_statistics())
+
+info = "Parameters: "
+stats = firstPopulation.get_brief_statistics()
+for gene in const.GENE_TYPES:
+    info += (gene + ": "+ str(stats[gene]) + ", ")
+
+generate_graphs.graph(healthy,infectious,sick,dead,recovered)
