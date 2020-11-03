6bd8fb678cb9c73e2c41896b3bb4fc4e2c354b57
maritoye <maritg@me.com>
2020-11-03 13:55:36 +0100

    minor fixes

--- generate_graphs.py
+++ generate_graphs.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 
 def graph(healthy, infectious, sick, dead, recovered):
 	"""
-	creates a graph of the states of all Persons in a population
-	:param healthy: list - numbers of healthy Persons per timestep
-	:param infectious: list - numbers of infectious Persons per timestep
-	:param sick: list - numbers of sick Persons per timestep
-	:param dead: list - numbers of dead Persons per timestep
-	:param recovered: list - numbers of recovered Persons per timestep
-	:return:
+	Creates a graph of the states of all Persons in a population
+	:param healthy: list - numbers of healthy Persons per time step
+	:param infectious: list - numbers of infectious Persons per time step
+	:param sick: list - numbers of sick Persons per time step
+	:param dead: list - numbers of dead Persons per time step
+	:param recovered: list - numbers of recovered Persons per time step
 	"""
 	x = [i for i in range(len(healthy))]
 
-	plt.plot(x, healthy, label="Healthy", color="gray")
+	plt.plot(x, healthy, color="gray", label="Healthy")
 	plt.plot(x, infectious, color="yellow", label="Infectious")
 	plt.plot(x, sick, color="red", label="Sick")
 	plt.plot(x, dead, color="black", label="Dead")
@@ -26,5 +25,5 @@ def graph(healthy, infectious, sick, dead, recovered):
 
 	plt.savefig("one_run_images/graph.png")
 
-	plt.show()
+	#plt.show()
 	plt.close()
